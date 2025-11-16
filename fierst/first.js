301f8997d1377cccb50776c9e39ad14bed58793a
amaan051003 <amaankhan06043@gmail.com>
2025-11-16 07:06:49 +0000

    a basic code for colour changing boxes

--- fierst/first.js
+++ fierst/first.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+function ramdoncolour() {
+    const ran=Math.ceil(Math.random()*255);
+    const ran1=Math.ceil(Math.random()*255);
+    const ran2=Math.ceil(Math.random()*255);
+    return `rgb(${ran},${ran1},${ran2})`;  
+}
+const boxes=document.querySelectorAll('.box');
+boxes.forEach(e => {
+      e.style.backgroundColor=ramdoncolour();  
+});
